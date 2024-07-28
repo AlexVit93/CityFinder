e5d5b83b0f0bb87e45ab88bb61c621d4eda4202e
Antuane_Specione <boosla@gmail.com>
2024-07-28 18:53:54 +0300

    feat: re-write config again 5

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
-import os
-
-API_TOKEN = os.getenv('API_TOKEN')
+API_TOKEN = "7402636958:AAEdQF221sx9u5TNyPNVn4mF5hDFl1z-wiI"
 REDIS_HOST = "redis.railway.internal"
 REDIS_PORT = 6379  
 REDIS_PASSWORD = "JvtKAapimpWMwQastKNsGqSutkWggzjb"  
-POSTGRES_URL = os.getenv('POSTGRES_URL')
+POSTGRES_URL = "postgresql://postgres:bDPDBfWQDTUtxcKaocZoUNFRbhPBxhSe@postgres.railway.internal:5432/railway"
 
 print(f"REDIS_HOST={REDIS_HOST}, REDIS_PORT={REDIS_PORT}, REDIS_PASSWORD={'yes' if REDIS_PASSWORD else 'no'}, POSTGRES_URL={POSTGRES_URL}")
