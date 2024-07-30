97c750b95cf61810f351f51c55fc2eeb83c3047c
Antuane_Specione <boosla@gmail.com>
2024-07-31 00:43:38 +0300

    feat: return del keys

--- config.py
+++ config.py
@@ -1,7 +1,3 @@
-API_TOKEN = "7402636958:AAEdQF221sx9u5TNyPNVn4mF5hDFl1z-wiI"
-REDIS_HOST = "redis.railway.internal"
-REDIS_PORT = 6379  
-REDIS_PASSWORD = "yQiWGxBMTJWMFJWxYVLuBZXvlCLtMIOj"  
-POSTGRES_URL = "postgresql://postgres:AxCymzjvzcXSDOMpQJgiNTmQzsGyKXdT@postgres.railway.internal:5432/railway"
+#I delete all keys
 
-print(f"REDIS_HOST={REDIS_HOST}, REDIS_PORT={REDIS_PORT}, REDIS_PASSWORD={'yes' if REDIS_PASSWORD else 'no'}, POSTGRES_URL={POSTGRES_URL}")
+# print(f"REDIS_HOST={REDIS_HOST}, REDIS_PORT={REDIS_PORT}, REDIS_PASSWORD={'yes' if REDIS_PASSWORD else 'no'}, POSTGRES_URL={POSTGRES_URL}")
