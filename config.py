fabe47eddc4f7c85839401bc8667843798441fd6
Antuane_Specione <boosla@gmail.com>
2024-07-31 00:01:23 +0300

    refactor: return keys

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
-#I delete all keys
+API_TOKEN = "7402636958:AAEdQF221sx9u5TNyPNVn4mF5hDFl1z-wiI"
+REDIS_HOST = "redis.railway.internal"
+REDIS_PORT = 6379  
+REDIS_PASSWORD = "JvtKAapimpWMwQastKNsGqSutkWggzjb"  
+POSTGRES_URL = "postgresql://postgres:bDPDBfWQDTUtxcKaocZoUNFRbhPBxhSe@postgres.railway.internal:5432/railway"
 
-# print(f"REDIS_HOST={REDIS_HOST}, REDIS_PORT={REDIS_PORT}, REDIS_PASSWORD={'yes' if REDIS_PASSWORD else 'no'}, POSTGRES_URL={POSTGRES_URL}")
+print(f"REDIS_HOST={REDIS_HOST}, REDIS_PORT={REDIS_PORT}, REDIS_PASSWORD={'yes' if REDIS_PASSWORD else 'no'}, POSTGRES_URL={POSTGRES_URL}")
